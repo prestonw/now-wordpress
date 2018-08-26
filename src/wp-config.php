5b024b669c9745760aff9c6952a1f4afff8f3e01
Will <preston.ie@me.com>
2018-08-26 13:52:34 +0100

    wp-config update for SSL over localhost

--- src/wp-config.php
+++ src/wp-config.php
@@ -6,11 +6,18 @@ define('DB_COLLATE', '');
 
 $table_prefix  = 'wp_';
 
-$protocol = 'https';
+$protocol = 'http';
 if( isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' ) {
   $_SERVER['HTTPS']='on';
   $protocol = 'https';
 }
+
+if($_SERVER["HTTPS"] != "on")
+{
+    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+    exit();
+}
+
 
 define('WP_SITEURL', $protocol . '://' . $_SERVER['HTTP_HOST']);
 define('WP_HOME', $protocol . '://' . $_SERVER['HTTP_HOST']);
